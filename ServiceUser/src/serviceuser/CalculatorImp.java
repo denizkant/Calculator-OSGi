d1d9f57526fe862a19c5286408e2686c8b90b1ea
Deniz Kantar <86944888+denizkant@users.noreply.github.com>
2024-05-15 17:57:38 +0300

    Update CalculatorImp.java

--- ServiceUser/src/serviceuser/CalculatorImp.java
+++ ServiceUser/src/serviceuser/CalculatorImp.java
@@ -5,19 +5,16 @@ import java.math.BigInteger;
 public class CalculatorImp implements Calculator {
 
 	public BigInteger add(BigInteger num1, BigInteger num2) {
-		// TODO Auto-generated method stub
 		return num1.add(num2);
 	}
 
 	
 	public BigInteger subtract(BigInteger num1, BigInteger num2) {
-		// TODO Auto-generated method stub
 		return num1.subtract(num2);
 	}
 
 	
 	public BigInteger multiply(BigInteger num1, BigInteger num2) {
-		// TODO Auto-generated method stub
 		return num1.multiply(num2);
 	}
 
